78c4e2531ce38c3e7aa3ef9620fc254d8ddd8612
unknowngmr02 <ggcytrguitt@gmail.com>
2025-02-06 16:37:48 +0530

    Update script.js to connect to render.com 2nd time with different external server

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Connect to WebSocket server
-const socket = io('https://chat-app-3-0zms.onrender.com'); // Make sure this matches your backend
+const socket = io('https://temp-chatting-server.onrender.com'); // Make sure this matches your backend
 
 let username = '';
 
